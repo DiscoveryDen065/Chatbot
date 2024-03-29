e31f0fe9228aa29d9439a251bc566f098b0feef9
DiscoveryDen065 <165190791+DiscoveryDen065@users.noreply.github.com>
2024-03-29 10:08:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from gemini_utility import load_gemini_pro_model
+
+#get the working directly
+working_directory=os.path.dirname(os.path.abspath(__file__))
+
+#setting the page configuration
+st.set_page_config(
+page_title="Gemini AI",
+page_icon= "🧠",
+layout="centered"
+)
+with st.sidebar:
+    selected=option_menu("Gemini AI",["Chatbot","Image Captioning","Embed Text","Ask me anything"],menu_icon='robot',icons=['chat-dots-fill','image','textarea-t','patch-question-fill'],default_index=0)
+
+#function to translate role between gemini pro and streamlit terminology
+def translate_role_for_streamlit(user_role):
+    if user_role == 'model':
+        return 'assistant'
+    else:
+        return user_role
+
+
+if selected == "Chatbot":
+
+    model = load_gemini_pro_model()
+
+    #Initialize chat session in streamlit if not already present
+    if "chat_session" not in st.session_state:
+        st.session_state.chat_session = model.start_chat(history=[])
+
+#streamlit page title
+    st.title("🤖 Chatbot")
+
+#display the chat history
+    for message in st.session_state.chat_session.history:
+        with st.chat_message(translate_role_for_streamlit(message.role)):
+            st.markdown(message.parts[0].text)
+
+#input field for user message
+    user_prompt = st.chat_input("Ask Gemini_pro...")
+
+    if user_prompt:
+        st.chat_message("user").markdown(user_prompt)
+        gemini_response = st.session_state.chat_session.send_message(user_prompt)
+
+        #display gemini-pro response
+        with st.chat_message("assistant"):
+            st.markdown(gemini_response.text)
